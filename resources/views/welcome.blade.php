badb38010199dca53324c5972a80b5046742ace0
M Pawit <muhammad.pawit93@gmail.com>
2024-11-27 10:29:23 +0700

    debug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 <div class="login-box">
     <div class="login-logo">
         <a href="#">
-            <b>Admin</b>LTE</a>
+            <b>Forboys</b>Production</a>
     </div>
     <!-- /.login-logo -->
     <div class="card">
